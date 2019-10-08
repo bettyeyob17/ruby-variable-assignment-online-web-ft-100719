4b39696ecb5ac0c1d88f17d7841421468314f7c0
Betial Eyob <bettyeyob24@gmail.com>
2019-10-08 16:15:13 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,3 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
+greeting= 'hello World' 
+# Define a local variable called 'greeting',
+ puts 'hello' # give it the value of the string "Hello World"
